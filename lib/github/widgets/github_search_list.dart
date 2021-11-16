685b2a84a2378c88977e088ae6a7e4fcc7feb483
Dean Rumen <53382410+drumen@users.noreply.github.com>
2021-11-16 01:47:12 +0100

    Correction for loading new items
    - in most cases scrolling to the end of the list and loading (querying) new items spent more than one query, in same case even the whole quota

--- lib/github/widgets/github_search_list.dart
+++ lib/github/widgets/github_search_list.dart
@@ -121,7 +121,7 @@ class _GitHubSearchListState extends State<GitHubSearchList> {
     if (!_scrollController.hasClients) return false;
     final maxScroll = _scrollController.position.maxScrollExtent;
     final currentScroll = _scrollController.offset;
-    return currentScroll >= (maxScroll * 0.9);
+    return currentScroll >= maxScroll;
   }
 
   Widget _searchTypeListItem(
